27055fc1507ca8dcc85f6eba0a0592ab82117254
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-03-23 14:32:39 +0100

    message

--- intro .sql
+++ intro .sql
@@ -172,7 +172,7 @@ select COUNT(distinct service) from employes;
 select service, count(id_employes) from employes group by service;
 -- 10. Afficher toutes les infos de l'employé du service commercial le mieux payé
 select * from employes where salaire = (select max(salaire) from employes) ;
-select * from employes order by salaire desc limit 0,1;
+select * from employes where service='commercial' order by salaire desc limit 0,1;
 -- 11. Afficher l'employé ayant été embauché en dernier
 select * from employes where date_embauche=(select max(date_embauche) from employes);
 select * from employes order by date_embauche desc limit 0,1;
